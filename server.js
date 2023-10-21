ad708fe8564a358b5514be0682f7d2bd019241bb
ExileWeirdo <shadowhunterswe@gmail.com>
2023-10-21 20:22:47 +0200

    small fixes

--- server.js
+++ server.js
@@ -186,6 +186,8 @@ const userSchema = new Schema({
 
 const User = mongoose.model('User', userSchema)
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+const PORT = process.env.PORT ||3000
+
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}`)
 });
