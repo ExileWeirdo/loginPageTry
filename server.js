7abc71305ab4190a49c24d0409e86078e29108a2
ExileWeirdo <shadowhunterswe@gmail.com>
2023-10-21 19:59:39 +0200

    Integrated mongoDB cluster

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
 const path = require("path");
-const fs = require("fs");
 const session = require("express-session");
+const mongoose = require('mongoose');
+const { error } = require("console");
+
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -21,7 +28,7 @@ app.use(
   }),
 );
 
-app.get('/', isAuthenticated, (req, res) => {
+app.get('/', isAuthenticated,  (req, res) => {
   res.render("index.ejs");
 });
 
@@ -29,226 +36,121 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {
-  fs.readFile("userInfo.txt", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-      return;
+app.post("/login", async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  try {
+    const authenticatedUser = await authenticateUser(username, password)
+
+    if (authenticatedUser){
+      req.session.authenticated = true
+      const Name = authenticatedUser.firstName
+      const surName = authenticatedUser.surName
+      const Username = authenticatedUser.username
+      const imageLink = authenticatedUser.profilePicture
+      const Email = authenticatedUser.email
+      const PostalCode = authenticatedUser.postalcode
+      const City = authenticatedUser.city
+      const Country = authenticatedUser.country
+      const Adress = authenticatedUser.adress
+      res.render('index.ejs', { Name, surName, Username, imageLink, Email, PostalCode, City, Country, Adress } )
     }
+  } catch (err) {
+    console.log(err)
 
-    const lines = data.split("\n");
-    let authenticated = false;
-
-    const username = req.body.username;
-    const password = req.body.password;
-
-    for (const line of lines) {
-      const [
-        savedName,
-        savedSurname,
-        savedUsername,
-        savedHashedPassword,
-        savedAdress,
-        savedImageSrc,
-        savedPostalCode,
-        savedCity,
-        savedEmail,
-        savedCountry,
-      ] = line.split(";");
-
-      console.log(`Comparing: ${username} with ${savedUsername}`);
-
-      if (username === savedUsername) {
-        bcrypt.compare(password, savedHashedPassword, (err, result) => {
-          if (err) {
-            console.error(err);
-            res.status(500).send("Internal Server Error brrr");
-            return;
-          }
-
-          if (result) {
-            req.session.authenticated = true;
-            const Name = savedName;
-            const surName = savedSurname;
-            const Adress = savedAdress;
-            const imageLink = savedImageSrc;
-            const PostalCode = savedPostalCode;
-            const Username = savedUsername;
-            const City = savedCity;
-            const Email = savedEmail;
-            const Country = savedCountry;
-            res.render("index.ejs", {
-              Name,
-              surName,
-              Adress,
-              imageLink,
-              PostalCode,
-              Username,
-              City,
-              Email,
-              Country,
-            });
-            return;
-          } else {
-            const errorMessage = "Incorrect password.";
-            res.status(401).render("login.ejs", { error: errorMessage });
-            return;
-          }
-        });
-        authenticated = true;
-        break;
-      }
+    if (err.message === 'User not found.'){
+      const errorMessage = 'User not found.'
+      res.render('login.ejs', { error: errorMessage })
+    } else if (err.message === 'Incorrect password.'){
+      const errorMessage = 'Incorrect password.'
+      res.render('login.ejs', { error: errorMessage })
+    } else {
+      const errorMessage = 'An error occured'
+      res.render('login.ejs', { error: errorMessage })
     }
-
-    if (!authenticated) {
-      const errorMessage = "User not found.";
-      res.status(401).render("login.ejs", { error: errorMessage });
-      return;
-    }
-  });
+  }
 });
+
+async function authenticateUser(username, password) {
+  const user = await User.findOne({ username: username})
+
+  if (!user){
+    throw new Error('User not found.')
+  }
+
+  const hashedPassword = user.password
+  const passwordMatch = await bcrypt.compare(password, hashedPassword)
+
+  if (passwordMatch) {
+    return user
+  } else {
+    throw new Error('Incorrect password.')
+  }
+}
+
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
 app.post("/register", async (req, res) => {
-  try {
-    const usernameExists = await userNameExistsInFile(req.body.username);
-
-    if (usernameExists) {
-      const errorMessage = "Username is already taken";
-      return res.render("register.ejs", { error: errorMessage });
-    }
-
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const name = req.body.name;
     const surname = req.body.surname;
     const username = req.body.username;
-    const password = hashedPassword;
     const adress = req.body.adress;
 
     if (req.body.selectedText === "Profile Picture") {
       const errorMessage = "Please pick a Profile Picture.";
-      res.status(401).render("register.ejs", { error: errorMessage });
-      return;
+      return res.status(401).render("register.ejs", { error: errorMessage });
     }
 
     const selectedProfileText = req.body.selectedText;
 
     const profileImages = {
-      Cat: "/images/catImagePic.jpg",
-      Dog: "/images/dogImagePic.jpg",
-      Monkey: "/images/monkeyImagePic.jpg",
-      Bunny: "/images/bunnyImagePic.jpg",
-      Rhino: "/images/rhinoImagePic.jpg",
-      Eagle: "/images/eagleImagePic.jpg",
+      Cat: "https://github.com/ExileWeirdo/loginPageTry/blob/b93c1f9232aa1878f8eb6be049df88f1d03bcdcd/public/images/catImagePic.jpg?raw=true",
+      Dog: "https://github.com/ExileWeirdo/loginPageTry/blob/b93c1f9232aa1878f8eb6be049df88f1d03bcdcd/public/images/dogImagePic.jpg?raw=true",
+      Monkey: "https://github.com/ExileWeirdo/loginPageTry/blob/b93c1f9232aa1878f8eb6be049df88f1d03bcdcd/public/images/monkeyImagePic.jpg?raw=true",
+      Bunny: "https://github.com/ExileWeirdo/loginPageTry/blob/d2e878c78aaa999b6e54b21527762efa7dfd1687/public/images/bunnyImagePic.jpg?raw=true",
+      Rhino: "https://github.com/ExileWeirdo/loginPageTry/blob/b93c1f9232aa1878f8eb6be049df88f1d03bcdcd/public/images/rhinoImagePic.jpg?raw=true",
+      Eagle: "https://github.com/ExileWeirdo/loginPageTry/blob/b93c1f9232aa1878f8eb6be049df88f1d03bcdcd/public/images/eagleImagePic.jpg?raw=true",
     };
 
-    const imageSrc =
-      profileImages[selectedProfileText] || "/images/catImagePic.jpg";
+    const imageSrc = profileImages[selectedProfileText] || "https://github.com/ExileWeirdo/loginPageTry/blob/b93c1f9232aa1878f8eb6be049df88f1d03bcdcd/public/images/catImagePic.jpg?raw=true";
     const postalCode = req.body.postalCode;
     const city = req.body.city;
     const email = req.body.email;
     const country = req.body.country;
 
-    const row =
-      name +
-      ";" +
-      surname +
-      ";" +
-      username +
-      ";" +
-      password +
-      ";" +
-      adress +
-      ";" +
-      imageSrc +
-      ";" +
-      postalCode +
-      ";" +
-      city +
-      ";" +
-      email +
-      ";" +
-      country +
-      "\n";
-    fs.appendFile("userInfo.txt", row, (err) => {
-      if (err) {
-        console.log("Error appending to userInfo.txt:", err);
-        return res.status(500).send("Internal Server Error");
-      }
+    User.findOne({ username: username })
+   .then((existingUser) => {
+    if (existingUser) {
+      const errorMessage = "Username already taken";
+      return res.render("register.ejs", { error: errorMessage });
+    }
 
-      console.log("User registered succesfully");
-      res.redirect("/login");
-    });
-    console.log(
-      "Name: " +
-        name +
-        "\n" +
-        "Surname: " +
-        surname +
-        "\n" +
-        "Username: " +
-        username +
-        "\n" +
-        "Password: " +
-        hashedPassword +
-        "\n" +
-        "Adress: " +
-        adress +
-        "\n" +
-        "Image: " +
-        selectedProfileText +
-        "\n" +
-        "PostalCode: " +
-        postalCode +
-        "\n" +
-        "City: " +
-        city +
-        "\n" +
-        "Email: " +
-        email +
-        "\n" +
-        "Country: " +
-        country,
-    );
-  } catch (error) {
-    console.log(error);
-    res.redirect("/register");
-  }
+    const newUser = new User({
+      firstName: name,
+      surName: surname,
+      username: req.body.username,
+      password: hashedPassword,
+      adress: adress,
+      profilePicture: imageSrc,
+      postalcode: postalCode,
+      city: city,
+      email: email,
+      country: country,
+    })
+
+    newUser.save()
+    return res.render('login.ejs')
+   })
+   .catch((err) => {
+    console.error(err)
+    return res.status(500).render('register.ejs', { error: 'Error while checking username availability'})
+   })
 });
-
-const userNameExistsInFile = (usernameToCheck) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile("./userInfo.txt", "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      if (data.trim() === "") {
-        resolve(false);
-        return;
-      }
-      const lowerCaseUsernameToCheck = usernameToCheck.toLowerCase();
-      const lines = data.split("\n");
-      for (const line of lines) {
-        const savedUsername = line.split(";")[2];
-        if (
-          savedUsername &&
-          savedUsername.trim().toLowerCase() === lowerCaseUsernameToCheck
-        ) {
-          resolve(true);
-          return;
-        }
-      }
-
-      resolve(false);
-    });
-  });
-};
 
 function isAuthenticated(req, res, next) {
   if (req.session.authenticated) {
@@ -267,4 +169,23 @@ app.get("/logout", (req, res) => {
   });
 });
 
-app.listen(3000);
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: String,
+  surName: String,
+  username: String,
+  password: String,
+  adress: String,
+  profilePicture: String,
+  postalcode: Number,
+  city: String,
+  email: String,
+  country: String,
+})
+
+const User = mongoose.model('User', userSchema)
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+});
